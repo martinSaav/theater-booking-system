32ea05dfd1567a28af5e1683d69339dc6c97b302
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2025-05-06 22:32:16 -0300

    first commit backend

---
+++ backend/theater-booking-system/src/main/java/com/theater/booking/TheaterBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.theater.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheaterBookingSystemApplication.class, args);
+    }
+
+}
---
+++ backend/theater-booking-system/src/test/java/com/theater/booking/TheaterBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theater.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
