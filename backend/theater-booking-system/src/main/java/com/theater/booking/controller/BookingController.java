df0c1d5f87d8fd485bde47f0d340b0549173ff62
Martin Saavedra <martin996@hotmail.com.ar>
2025-05-09 15:53:44 -0300

    delego exceptions a controller advice y update necesita si o si un id en la url

--- backend/theater-booking-system/src/main/java/com/theater/booking/controller/BookingController.java
+++ backend/theater-booking-system/src/main/java/com/theater/booking/controller/BookingController.java
@@ -3,7 +3,13 @@ package com.theater.booking.controller;
 
 import com.theater.booking.dto.BookingRequestDTO;
 import com.theater.booking.dto.BookingResponseDTO;
+import com.theater.booking.exceptions.BookingNotFoundException;
+import com.theater.booking.exceptions.EventNotFoundException;
+import com.theater.booking.exceptions.NotValidBodyException;
+import com.theater.booking.exceptions.UnknownErrorException;
 import com.theater.booking.service.BookingService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,26 +29,56 @@ public class BookingController {
 
     @GetMapping("")
     public ResponseEntity<List<BookingResponseDTO>> getAllRecord() {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+        } catch (Exception e) {
+            throw new UnknownErrorException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<BookingResponseDTO> getRecordById(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+        } catch (EntityNotFoundException e) {
+            throw new BookingNotFoundException(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new UnknownErrorException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("")
     public ResponseEntity<BookingResponseDTO> save(@RequestBody BookingRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(dto));
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.save(dto));
+        } catch (DataIntegrityViolationException e) {
+            throw new NotValidBodyException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            throw new UnknownErrorException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping(value = {"", "/{id}"})
+    @PutMapping( "/{id}")
     public ResponseEntity<BookingResponseDTO> update(@PathVariable(required = false) Long id, @RequestBody BookingRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.update(id, dto));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.update(id, dto));
+        } catch (DataIntegrityViolationException e) {
+            throw new NotValidBodyException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (EntityNotFoundException e) {
+            throw new BookingNotFoundException(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new UnknownErrorException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
+        try {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(service.delete(id));
+        } catch (EntityNotFoundException e) {
+            throw new EventNotFoundException(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new UnknownErrorException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
