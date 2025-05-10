aae9afc89da51dbcc466be5763a91e8ea4d07381
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2025-05-10 14:58:39 -0300

    crear evento y reserva de forma basica como ejemplo

--- frontend/teatro-gran-espectaculo-front/src/app/booking/booking-list/booking-list.component.ts
+++ frontend/teatro-gran-espectaculo-front/src/app/booking/booking-list/booking-list.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './booking-list.component.html',
   styleUrl: './booking-list.component.scss'
 })
 export class BookingListComponent {
+  customerName = '';
+  eventName = '';
 
+  bookings = [
+    { customer: 'martin', event: 'a' }
+  ];
+
+  createBooking() {
+    this.bookings.push({
+      customer: this.customerName,
+      event: this.eventName
+    });
+    this.customerName = '';
+    this.eventName = '';
+  }
 }
--- frontend/teatro-gran-espectaculo-front/src/app/event/event-list/event-list.component.ts
+++ frontend/teatro-gran-espectaculo-front/src/app/event/event-list/event-list.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.scss'
 })
 export class EventListComponent {
+  eventName = '';
+  eventDate = '';
+  eventDescription = '';
 
+  events = [
+    { name: 'Evento de ejemplo', date: '2025-10-01', description: 'mmm' }
+  ];
+
+  createEvent() {
+    this.events.push({
+      name: this.eventName,
+      date: this.eventDate,
+      description: this.eventDescription
+    });
+    this.eventName = '';
+    this.eventDate = '';
+    this.eventDescription = '';
+  }
 }
