fd601fc90b02495840eeff3d1c1265d25a2957b0
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2025-05-11 08:24:48 -0300

    verifico que los eventos se devuelvan bien

--- backend/theater-booking-system/src/test/java/com/theater/booking/controller/EventControllerTest.java
+++ backend/theater-booking-system/src/test/java/com/theater/booking/controller/EventControllerTest.java
@@ -2,10 +2,7 @@ package com.theater.booking.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.theater.booking.dto.*;
-import com.theater.booking.model.Concert;
-import com.theater.booking.model.Talk;
-import com.theater.booking.model.TheaterPlay;
-import com.theater.booking.model.Ticket;
+import com.theater.booking.model.*;
 import com.theater.booking.service.ConcertService;
 import com.theater.booking.service.EventService;
 import com.theater.booking.service.TalkService;
@@ -30,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest({EventController.class, TheaterPlayController.class, TalkController.class, ConcertController.class})
 @ActiveProfiles("test")
-public class EventControllerTest {
+class EventControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -51,7 +48,7 @@ public class EventControllerTest {
     private ConcertService concertService;
 
     @Test
-    public void testCreateTheaterPlay() throws Exception {
+    void testCreateTheaterPlay() throws Exception {
         TheaterPlayRequestDTO request = new TheaterPlayRequestDTO(
                 "Tech Talk",
                 LocalDateTime.of(2025, 1, 1, 0, 0),
@@ -71,16 +68,19 @@ public class EventControllerTest {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1L))
-                .andExpect(jsonPath("$.name").value("Tech Talk"));
+                .andExpect(jsonPath("$.name").value("Tech Talk"))
+                .andExpect(jsonPath("$.dateTime").value("2025-01-01T00:00"))
+                .andExpect(jsonPath("$.description").value("A talk about tech."))
+        ;
 
         verify(theaterPlayService, times(1)).save(any(TheaterPlayRequestDTO.class));
     }
 
     @Test
-    public void testCreateTalk() throws Exception {
-        TalkRequestDTO request = new TalkRequestDTO("Java Conf", LocalDateTime.of(2025, 5, 10, 18, 0), "Conference");
+    void testCreateTalk() throws Exception {
+        TalkRequestDTO request = new TalkRequestDTO("Java Conf", LocalDateTime.of(2025, 5, 10, 18, 0), "A conference");
         Talk talk = new Talk();
-        talk.setId(2L);
+        talk.setId(1L);
         talk.setName(request.getName());
         talk.setDateTime(request.getDateTime());
         talk.setDescription(request.getDescription());
@@ -92,17 +92,19 @@ public class EventControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(2L))
-                .andExpect(jsonPath("$.name").value("Java Conf"));
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Java Conf"))
+                .andExpect(jsonPath("$.dateTime").value("2025-05-10T18:00"))
+                .andExpect(jsonPath("$.description").value("A conference"));
 
         verify(talkService, times(1)).save(any(TalkRequestDTO.class));
     }
 
     @Test
-    public void testCreateConcert() throws Exception {
+    void testCreateConcert() throws Exception {
         ConcertRequestDTO request = new ConcertRequestDTO("Live Rock", LocalDateTime.of(2025, 6, 15, 21, 0), "Rock concert");
         Concert concert = new Concert();
-        concert.setId(3L);
+        concert.setId(1L);
         concert.setName(request.getName());
         concert.setDateTime(request.getDateTime());
         concert.setDescription(request.getDescription());
@@ -114,30 +116,52 @@ public class EventControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(3L))
-                .andExpect(jsonPath("$.name").value("Live Rock"));
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Live Rock"))
+                .andExpect(jsonPath("$.dateTime").value("2025-06-15T21:00"))
+                .andExpect(jsonPath("$.description").value("Rock concert"));
 
         verify(concertService, times(1)).save(any(ConcertRequestDTO.class));
     }
 
     @Test
-    public void testGetAllEvents() throws Exception {
+    void testGetAllEvents() throws Exception {
         List<Ticket> tickets = new ArrayList<>();
+        List<EventResponseDTO> events = new ArrayList<>();
         TheaterPlay theaterPlay = new TheaterPlay();
         theaterPlay.setId(1L);
         theaterPlay.setName("Tech Talk");
         theaterPlay.setDateTime(LocalDateTime.of(2025, 1, 1, 1, 0));
         theaterPlay.setDescription("A talk about tech.");
         theaterPlay.setTickets(tickets);
+        EventResponseDTO theaterPlayResponse = new EventResponseDTO(theaterPlay);
+        events.add(theaterPlayResponse);
+        Talk talk = new Talk();
+        talk.setId(2L);
+        talk.setName("Java Conf");
+        talk.setDateTime(LocalDateTime.of(2025, 5, 10, 18, 0));
+        talk.setDescription("A conference");
+        talk.setTickets(tickets);
+        EventResponseDTO talkResponse = new EventResponseDTO(talk);
+        events.add(talkResponse);
+        Concert concert = new Concert();
+        concert.setId(3L);
+        concert.setName("Live Rock");
+        concert.setDateTime(LocalDateTime.of(2025, 6, 15, 21, 0));
+        concert.setDescription("Rock concert");
+        concert.setTickets(tickets);
+        EventResponseDTO concertResponse = new EventResponseDTO(concert);
+        events.add(concertResponse);
 
-        EventResponseDTO response = new EventResponseDTO(theaterPlay);
-
-        when(eventService.findAll()).thenReturn(List.of(response));
+        when(eventService.findAll()).thenReturn(events);
 
         mockMvc.perform(get("/api/v1/events"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(1L))
-                .andExpect(jsonPath("$[0].name").value("Tech Talk"));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()").value(3))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[2].id").value(3));
 
         verify(eventService, times(1)).findAll();
     }
