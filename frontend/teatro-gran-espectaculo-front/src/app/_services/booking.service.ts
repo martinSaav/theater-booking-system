1cea932bedda094802b5cc5d206bc6228274bb3f
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2025-05-12 21:32:16 -0300

    uso de api subida a aws y con enviroments para local y produccion

--- frontend/teatro-gran-espectaculo-front/src/app/_services/booking.service.ts
+++ frontend/teatro-gran-espectaculo-front/src/app/_services/booking.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Booking } from '../_models/booking';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
-private http = inject(HttpClient);
-  private baseUrl = 'http://localhost:8080/api/v1/bookings';
-
+  
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiBaseUrl}/bookings`;
   getAll() {
     return this.http.get<Booking[]>(this.baseUrl);
   }
--- frontend/teatro-gran-espectaculo-front/src/app/_services/event.service.ts
+++ frontend/teatro-gran-espectaculo-front/src/app/_services/event.service.ts
@@ -2,14 +2,14 @@ import { Injectable} from '@angular/core';
 import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Event } from '../_models/event';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-private http = inject(HttpClient);
-
-  private baseUrl = 'http://localhost:8080/api/v1/events';
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiBaseUrl}/events`;
 
   getAllEvents() {
     return this.http.get<Event[]>(this.baseUrl);
